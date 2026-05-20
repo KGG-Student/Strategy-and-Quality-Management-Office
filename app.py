29fd26c2635ea7a562c5eb553e84d366e685dba3
Kyle Galanida <20210022244@my.xu.edu.ph>
2026-05-20 17:30:17 +0800

    "Prepare app for Render deployment"

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def api_delete_program(program_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
